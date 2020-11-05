776aac9d57630991d98aba61fe5f0345caefdfae
Mattie Lee <57819411+mattiel@users.noreply.github.com>
2020-11-05 15:35:02 +0900

    added swiper and glyphs

---
+++ js/index.js
@@ -0,0 +1,6 @@
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.taa-dashboard-date-selector');  
+});
